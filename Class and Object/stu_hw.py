585f1b5f357da3fa56b90838e5c385e08329039a
ziqian9030 <ziqian9030@gmail.com>
2026-09-12 13:22:01 +0700

    hw done

---
+++ Class and Object/stu_hw.py
@@ -0,0 +1,24 @@
+class Student:
+    id = ""
+    name = ""
+    age = ""
+    gender = ""
+    average = ""
+
+    def input(self):
+        self.id =int(input(" Enter your ID: "))
+        self.name=input("Enter your name: ")
+        self.age=int(input("Enter your age: "))
+        self.gender=input("Enter gender: ")
+        self.average=int(input(" Enter your average score: "))
+
+    def show(self):
+        print("ID: ",self.id)
+        print("Name: ",self.name)
+        print("Age: ",self.age)
+        print("Gender: ",self.gender)
+        print("Average: ",self.average)
+
+student1 = Student()
+student1.input()
+student1.show()
